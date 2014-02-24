5b26596998d4c6fab3086b651e697f6ec72334fd
Kacy Humphrey <khumphrey@etsms.com>
2014-02-24 17:18:39 -0500

    fix for launchie

--- app.js
+++ app.js
@@ -1,22 +1,20 @@
-// require( './lib' )
-// 	.set( 'httpPort', 8787 )
-// 	.start();
+require( './lib' )
+	.set( 'httpPort', 8787 )
+	.start();
 
-var Launchie = require('./lib/Launchie.js').Launchie;
+// var Launchie = require('./lib/Launchie.js').Launchie;
+
+// var launchie = new Launchie();
 
 
 
-var baseURL = 'https://www.google.com/search?q='
+// var baseURL = 'https://www.google.com/search?q='
 
-var search = ['TRANSIT LABS','CollegeSnapps','Degree Prospects','Edbacker','Flat World Knowledge','Flat World Knowledge, Inc.','Linked Senior','TypingClub','VentureBoard','Vibeffect','visitdays'];
+// var search = ['TRANSIT LABS','CollegeSnapps','Degree Prospects','Edbacker','Flat World Knowledge','Flat World Knowledge, Inc.','Linked Senior','TypingClub','VentureBoard','Vibeffect','visitdays'];
 
-for (var i = 0; i < search.length; i++) {
-	var temp = search[i].replace(' ', '+');
+// for (var i = 0; i < search.length; i++) {
+// 	var temp = search[i].replace(' ', '+');
 
-	console.log(temp);
-
-	var launchie = new Launchie();
-
-	launchie.listen("launch " + baseURL + temp + "+founder");
-};
+// 	launchie.listen("launch " + baseURL + temp + "+founder");
+// };
 
